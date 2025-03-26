5b3a0a1ade4472c84c1047d0320e36b96b973b32
Ayhamkhas <jlassi.ayhem2002@gmail.com>
2025-03-26 14:41:23 +0300

    encoding moves added for game information aka move list

--- README.md
+++
@@ -1,2 +0,0 @@
-# Smart-Chess
-Chess engine prepared to compete on a high level with chess players 
--- smartchess.cpp
+++ smartchess.cpp
@@ -37,7 +37,7 @@ const char* empty_board = "8/8/8/8/8/8/8/8 w - - ";
 const char* start_position = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1 ";
 const char* tricky_position = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1 ";
 const char* killer_position = "rnbqkb1r/pp1p1pPp/8/2p1pP2/1P1P4/3P3P/P1P1P3/RNBQKBNR w KQkq e6 0 1";
-const char* cmk_position = "r2q1rk1/ppp2ppp/2n1bn2/2b1p3/3pP3/3P1NPP/PPP1NPB1/R1BQ1RK1 b - - 0 9 ";
+const char* cmk_position = "r2q1rk1/ppp2ppp/2n1bn2/2b1p3/3pP3/3P1NPP/PPP1NPB1/R1BQ1RK1 w - - 0 9 ";
 
 /****************************\
 
@@ -1599,16 +1599,70 @@ static inline void generate_moves()
 }
 
 
+/* This will be used to extract information for a specifific move
+          binary move bits                               hexidecimal constants
+    
+    0000 0000 0000 0000 0011 1111    source square       0x3f
+    0000 0000 0000 1111 1100 0000    target square       0xfc0
+    0000 0000 1111 0000 0000 0000    piece               0xf000
+    0000 1111 0000 0000 0000 0000    promoted piece      0xf0000
+    0001 0000 0000 0000 0000 0000    capture flag        0x100000
+    0010 0000 0000 0000 0000 0000    double push flag    0x200000
+    0100 0000 0000 0000 0000 0000    enpassant flag      0x400000
+    1000 0000 0000 0000 0000 0000    castling flag       0x800000
+*/
+
+#define encode_move(source, target, piece, promoted, capture, double_push, enpassant, castling) (source | (target << 6) | (piece << 12) | (promoted << 16) | (capture << 20) | (double_push << 21) | (enpassant << 22) | (castling << 23))
+
+// extract source square
+#define get_move_source(move) (move & 0x3f)
+
+// extract target square
+#define get_move_target(move) ((move & 0xfc0) >> 6)
+
+// extract piece
+#define get_move_piece(move) ((move & 0xf000) >> 12)
+
+// extract promoted piece
+#define get_move_promoted(move) ((move & 0xf0000) >> 16)
+
+// extract capture flag
+#define get_move_capture(move) (move & 0x100000)
+
+// extract double pawn push flag
+#define get_move_double(move) (move & 0x200000)
+
+// extract enpassant flag
+#define get_move_enpassant(move) (move & 0x400000)
+
+// extract castling flag
+#define get_move_castling(move) (move & 0x800000)
+
+
 
 int main()
 {
-    init_all();
-    // parse custom FEN string
-    parse_fen(cmk_position);
-    print_board();
+   // init all
+   init_all();
     
-    // generate moves
-    generate_moves();
-
-    return 0;
+   // create move
+   int move = encode_move(d7, e8, P, Q, 1, 0, 0, 0);
+   
+   // exract move items
+   int source_square = get_move_source(move);
+   int target_square = get_move_target(move);
+   int piece = get_move_piece(move);
+   int promoted_piece = get_move_promoted(move);
+   
+   // print move items
+   cout <<"source square: "<< coordinates[source_square] << '\n';
+   cout <<"target square: "<< coordinates[target_square] << '\n';
+   cout <<"piece: "<< coordinates[piece] << '\n';
+   cout <<"promoted piece: "<< coordinates[promoted_piece] << '\n';
+   cout << "capture flag: " << ( get_move_capture(move) ? 1 : 0 )<< '\n';
+   cout << "double pawn push flag:  " << (get_move_double(move) ? 1 : 0) << '\n'; 
+   cout << "enpassant flag:  " << (get_move_enpassant(move) ? 1 : 0) << '\n';
+   cout << "castling flag:  " << (get_move_castling(move) ? 1 : 0) ;
+   
+   return 0;
 }
